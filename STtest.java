7b34f9e59d3167f6f89c257eda89d185abae85d4
PrashantSingh64 <104351789+PrashantSingh64@users.noreply.github.com>
2023-10-23 14:28:49 +0530

    Update STtest.java

--- STtest.java
+++ STtest.java
@@ -1,52 +1,78 @@
-package testing;
-
-
-
-public class Calculation {
-    public static void main(String[] args) {
-       
-        int numSubjects = 3;
-
-        
-
-        System.out.print("Enter student roll number: ");
-        int rollNumber = scanner.nextInt();
-
-        System.out.print("Enter student name: ");
-        String name = scanner.next();
-
-        int[] marks = new int[numSubjects];
-        double totalMarks = 0;
-
-        for (int i = 0; i < numSubjects; ++i) {
-            do {
-                System.out.print("Enter marks for subject " + (i + 1) + ": ");
-                marks[i] = scanner.nextInt();
-                if (marks[i] < 0 || marks[i] > maxMarks) {
-                    System.out.println("Invalid marks. Marks should be between 0 and " + maxMarks);
-                }
-            } while (marks[i] < 0 || marks[i] > maxMarks);
-            totalMarks += marks[i];
-        }
-
-        double percentage = (totalMarks / (numSubjects * maxMarks)) * 100;
-
-        System.out.println("\nTotal Marks: " + totalMarks + " out of " + (numSubjects * maxMarks));
-        System.out.println("Percentage: " + percentage + "%");
-
-        if (percentage < 45) {
-            System.out.println("Result: FF");
-        } else if (percentage >= 45 && percentage < 60) {
-            System.out.println("Result: Second Class");
-        } else if (percentage >= 60 && percentage < 75) {
-            System.out.println("Result: First Class");
-        } else if (percentage >= 75 && percentage < 90) {
-            System.out.println("Result: Distinction");
-        } else {
-            System.out.println("Result: Merit");
-        }
-
-        scanner.close();
-    }
+provider "alicloud" {
+  access_key = "LTAI5t6jen4QXx2UFBFEigGJ"
+  secret_key = "eexR6CvzcN7lCmdelclUd6HxAMr5OF"
+  region     = "cn-beijing"
 }
 
+resource "random_uuid" "cpu" {}
+
+# Define the resources to monitor
+resource "alicloud_cms_group_metric_rule" "CPU_Rul" {
+  group_id    = "173445410"
+  rule_id     = random_uuid.cpu.id
+  category    = "ecs"
+  namespace   = "acs_ecs_dashboard"
+  metric_name = "cpu_total"
+  period      = "60"
+
+  group_metric_rule_name = "CPU_Usage1"
+  contact_groups         = "ECS-test1"
+  escalations {
+    warn {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = "80"
+      times               = 3
+    }
+  }
+}
+resource "random_uuid" "Disk" {}
+resource "alicloud_cms_group_metric_rule" "Disk" {
+  group_id    = "173445410"
+  rule_id     = random_uuid.Disk.id
+  category    = "ecs"
+  namespace   = "acs_ecs_dashboard"
+  metric_name = "diskusage_total"
+  period      = "60"
+
+  group_metric_rule_name = "High_Disk_Usage"
+  contact_groups         = "ECS-test1"
+  escalations {
+    warn {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = "85"
+      times               = 3
+    }
+  }
+}
+resource "random_uuid" "Memory" {}
+resource "alicloud_cms_group_metric_rule" "Memory" {
+  group_id    = "173445410"
+  rule_id     = random_uuid.Memory.id
+  category    = "ecs"
+  namespace   = "acs_ecs_dashboard"
+  metric_name = "memoryusage_total"
+  period      = "60"
+
+  group_metric_rule_name = "High_Memory_Usage"
+  contact_groups         = "ECS-test1"
+  escalations {
+    warn {
+      comparison_operator = "GreaterThanOrEqualToThreshold"
+      statistics          = "Average"
+      threshold           = "80"
+      times               = 3
+    }
+  }
+}
+resource "alicloud_cms_alarm_contact" "alertcontact" {
+  alarm_contact_name = "ECS-monitor"
+  describe           = "For Test"
+  channels_mail      = "Prashant.Singh@genmills.com"
+  lang               = "en"
+}
+resource "alicloud_cms_alarm_contact_group" "alertgroup" {
+  alarm_contact_group_name = "ECS-test1"
+  contacts                 = [alicloud_cms_alarm_contact.alertcontact.alarm_contact_name]
+}
